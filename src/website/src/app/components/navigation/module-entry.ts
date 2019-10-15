fd63bed05bb3af57ffbd6f9c5d2ff6e0ecfc1938
EBNE <extern.markus.ebner@porscheinformatik.at>
2019-10-15 10:16:38 +0200

    remove unnecessary code

--- src/website/src/app/components/navigation/module-entry.ts
+++
@@ -1,9 +0,0 @@
-export interface ModuleEntry {
-  name: string;
-  i18n: string;
-  matIcon: string;
-  homeRoute: string;
-  url?: string;
-  descriptionI18n?: string;
-  category?: string;
-}
--- src/website/src/app/components/navigation/navigation-entry.ts
+++
@@ -1,9 +0,0 @@
-export interface NavigationEntry {
-  name: string;
-  i18n: string;
-  matIcon: string;
-  route?: string;
-  roles?: string[];
-  children?: NavigationEntry[];
-  showChildren?: boolean;
-}
