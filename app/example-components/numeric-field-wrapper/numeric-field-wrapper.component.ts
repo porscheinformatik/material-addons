d574286b70dd34725b10317839c24a2c145e275c
Stefan Lässer <stefan.laesser@porscheinformatik.com>
2020-09-01 13:55:08 +0200

    Auto-generated commit

---
+++ app/example-components/numeric-field-wrapper/numeric-field-wrapper.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-numeric-field-wrapper',
+  templateUrl: './numeric-field-wrapper.component.html',
+  styleUrls: ['./numeric-field-wrapper.component.scss'],
+})
+export class NumericFieldWrapperComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      moneyAmount: new FormControl(undefined, { validators: [Validators.required, Validators.min(0)], updateOn: 'blur' }),
+      percentage: new FormControl(undefined, { validators: [Validators.min(0), Validators.max(100)], updateOn: 'blur' }),
+    });
+  }
+}
