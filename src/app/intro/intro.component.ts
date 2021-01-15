696687b036cd573ffbef9a67a12d4949a41daebb
Stefan Lässer <stefan.laesser@porscheinformatik.com>
2021-01-15 09:33:39 +0100

    Updated copyright in demo

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss'],
 })
-export class IntroComponent {}
+export class IntroComponent {
+
+  public currentYear(): number {
+    return new Date().getFullYear();
+  }
+
+}
