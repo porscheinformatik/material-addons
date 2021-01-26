ccf506f99c427fdcade6074b1bccbd98258368b7
Stefan Laesser <stefan.laesser@porscheinformatik.com>
2021-01-26 10:10:23 +0100

    Auto-generated commit

--- app/example-components/read-only-field/read-only-field.component.ts
+++ app/example-components/read-only-field/read-only-field.component.ts
@@ -9,4 +9,7 @@ export class ReadOnlyFieldComponent {
   nullValue = null;
   undefinedValue: string;
   zeroValue = 0;
+  demoText =
+    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore \n' +
+    'et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ';
 }
