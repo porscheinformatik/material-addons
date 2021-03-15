df7a9c5309e1513678fc2623950d1b3c920ce5f0
Stefan Laesser <stefan.laesser@porscheinformatik.com>
2021-03-15 13:59:14 +0100

    Improved throttle button demo

--- src/app/example-components/throttle-click/throttle-click.component.ts
+++ src/app/example-components/throttle-click/throttle-click.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-throttle-click',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./throttle-click.component.scss'],
 })
 export class ThrottleClickComponent {
-  onButtonClick(): void {}
+
+  counter = 0;
+  counter2 = 0;
+
+  onButtonClick(): void {
+    this.counter = this.counter + 1;
+  }
+
+  onButtonClick2(): void {
+    this.counter2 = this.counter2 + 1;
+  }
 }
