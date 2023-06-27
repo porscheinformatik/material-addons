0892ac4828882b4619e7d64fa8e40a09a00507c4
Stefan Lässer <stefan.laesser@porscheinformatik.com>
2023-06-27 08:23:53 +0200

    Fix theming: Use outline as default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,12 @@ import { LinkCardComponent } from './home/link-card/link-card.component';
 import { HomeComponent } from './home/home.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
+import {MAT_CARD_CONFIG, MatCardModule} from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -222,7 +222,10 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     DataTableDevModule,
     MatChipsModule,
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}},
+    {provide: MAT_CARD_CONFIG, useValue: {appearance: 'outlined'}}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
