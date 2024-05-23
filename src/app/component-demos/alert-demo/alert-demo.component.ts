e3d3e3e459b2387350e342530652da8c5cd1dedb
Yanek Voloshchuk <yanek.voloshchuk.ext@porscheinformatik.com>
2024-05-23 14:54:37 +0200

    add api spec table to alert docs, change styles

--- src/app/component-demos/alert-demo/alert-demo.component.ts
+++ src/app/component-demos/alert-demo/alert-demo.component.ts
@@ -4,11 +4,12 @@ import { TextCodeComponent } from '../../components/text-code/text-code.componen
 import { ExampleViewerComponent } from '../../components/example-viewer/example-viewer.component';
 import { Example } from '../../components/example-viewer/example.class';
 import { AlertsComponent } from '../../example-components/alerts/alerts.component';
+import { AlertDemoApiSpecComponent } from './alert-demo-api-spec/alert-demo-api-spec.component';
 
 @Component({
   selector: 'app-alert-demo',
   standalone: true,
-  imports: [CommonModule, TextCodeComponent, ExampleViewerComponent],
+  imports: [CommonModule, TextCodeComponent, ExampleViewerComponent, AlertDemoApiSpecComponent],
   templateUrl: './alert-demo.component.html',
   styleUrl: './alert-demo.component.scss',
 })
