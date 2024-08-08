a56b6da1bc41518e111b924a87e9a644f1d1edd2
Stefan Lässer <stefan.laesser@porscheinformatik.com>
2024-08-08 14:28:07 +0200

    NumberFormat Service: Set NumberFormatService to be providedIn root

--- projects/material-addons/src/lib/numeric-field/numeric-field.module.ts
+++ projects/material-addons/src/lib/numeric-field/numeric-field.module.ts
@@ -1,17 +1,14 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { NumericFieldDirective } from './numeric-field.directive';
-import { NumberFormatService } from './number-format.service';
 
 @NgModule({
   imports: [NumericFieldDirective],
-  providers: [NumberFormatService],
   exports: [NumericFieldDirective],
 })
 export class NumericFieldModule {
   static forRoot(): ModuleWithProviders<NumericFieldModule> {
     return {
       ngModule: NumericFieldModule,
-      providers: [NumberFormatService],
     };
   }
 }
