d6ca64e4d2eaf00ea7579d4e9f159db2db6d29db
Yanek Voloshchuk <yanekvoloschuk@gmail.com>
2026-01-12 12:28:17 +0100

    fixes for tests configs

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'jest';
 import { createCjsPreset } from 'jest-preset-angular/presets/index.js';
 
 export default {
-  ...createCjsPreset(),
+  ...createCjsPreset({ tsconfig: '<rootDir>/tsconfig.jest.json' }),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
 } satisfies Config;
