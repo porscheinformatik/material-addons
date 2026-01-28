52acd09b03cc868cac79942acca880fd8229856e
lisakirchhofer <lisa.kirchhofer.ext@porscheinformatik.com>
2026-01-28 13:04:17 +0100

    upgrade dependency versions

--- jest.config.ts
+++ jest.config.ts
@@ -5,4 +5,8 @@ export default {
   ...createCjsPreset({ tsconfig: '<rootDir>/tsconfig.jest.json' }),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  // uuid > version 8 is ESM only and therefore does not transpile without this config
+  transformIgnorePatterns: [
+    'node_modules/(?!(uuid)/)',
+  ],
 } satisfies Config;
