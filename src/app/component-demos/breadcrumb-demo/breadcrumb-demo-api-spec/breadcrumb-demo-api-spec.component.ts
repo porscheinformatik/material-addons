5ac9c6176ec409405223af18248974761bd75dc0
Yanek Voloshchuk <yanek.voloshchuk.ext@porscheinformatik.com>
2026-03-13 15:36:48 +0100

    conflicts fixed

---
+++ src/app/component-demos/breadcrumb-demo/breadcrumb-demo-api-spec/breadcrumb-demo-api-spec.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumb-demo-api-spec',
+  imports: [],
+  templateUrl: './breadcrumb-demo-api-spec.component.html',
+  styleUrl: './breadcrumb-demo-api-spec.component.scss',
+})
+export class BreadcrumbDemoApiSpecComponent {
+
+}
