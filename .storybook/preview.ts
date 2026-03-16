ac67a13fd3c4dc4523356b81d3f7861e452b2afe
Yanek Voloshchuk <yanek.voloshchuk@dipocket.org>
2026-03-16 18:37:00 +0100

    fix formatting

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,14 @@
-import type { Preview } from '@storybook/angular'
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import docJson from "../documentation.json";
+import type { Preview } from '@storybook/angular';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
 setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
