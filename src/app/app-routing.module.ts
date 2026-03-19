104341ed96c8a8352577909a4a9f56bc445e65ef
Yanek Voloshchuk <yanek.voloshchuk@dipocket.org>
2026-03-19 12:42:35 +0100

    prettier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -198,7 +198,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
