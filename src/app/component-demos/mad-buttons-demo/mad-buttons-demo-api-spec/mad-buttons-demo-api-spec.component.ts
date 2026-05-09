c546378c14e3aa2e64b4a100820471b02d3cffea
Yanek Voloshchuk <yanek.voloshchuk@dipocket.org>
2026-05-09 19:01:01 +0200

    buttons refactored demo page added

---
+++ src/app/component-demos/mad-buttons-demo/mad-buttons-demo-api-spec/mad-buttons-demo-api-spec.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mad-buttons-demo-api-spec',
+  imports: [],
+  templateUrl: './mad-buttons-demo-api-spec.component.html',
+  styleUrl: './mad-buttons-demo-api-spec.component.scss',
+})
+export class MadButtonsDemoApiSpecComponent {}
---
+++ src/app/component-demos/mad-buttons-demo/mad-buttons-demo-color-spec/mad-buttons-demo-color-spec.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mad-buttons-demo-color-spec',
+  imports: [],
+  templateUrl: './mad-buttons-demo-color-spec.component.html',
+  styleUrl: './mad-buttons-demo-color-spec.component.scss',
+})
+export class MadButtonsDemoColorSpecComponent {}
