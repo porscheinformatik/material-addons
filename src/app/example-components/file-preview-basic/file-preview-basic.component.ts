4de306379dcae105727e2bfe8bc8557afe460c3e
Salman <mohammed.salman.ext@porscheinformatik.com>
2026-06-03 13:48:12 +0530

    Added File Preview Reusable Compoent for ('jpg', 'jpeg', 'png', 'gif', 'webp', 'bmp', 'svg', 'ico', 'pdf', 'docx', 'xlsx', 'xls', 'xlsm', 'xlsb', 'ods', 'csv') types

--- src/app/example-components/file-preview-basic/file-preview-basic.component.ts
+++ src/app/example-components/file-preview-basic/file-preview-basic.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { FileUploadComponent, FilePreviewComponent } from '@porscheinformatik/material-addons';
 import { FilePreviewItem, FilePreviewConfig, ThumbnailSize } from '@porscheinformatik/material-addons';
@@ -10,7 +11,7 @@ import { FilePreviewItem, FilePreviewConfig, ThumbnailSize } from '@porscheinfor
   selector: 'app-file-preview-basic',
   templateUrl: './file-preview-basic.component.html',
   styleUrl: './file-preview-basic.component.scss',
-  imports: [FilePreviewComponent, FileUploadComponent, MatCheckboxModule, MatButtonToggleModule, MatSlideToggleModule, FormsModule],
+  imports: [FilePreviewComponent, FileUploadComponent, MatCheckboxModule, MatButtonToggleModule, MatSlideToggleModule, MatCardModule, FormsModule],
 })
 export class FilePreviewBasicComponent {
   showDeleteAction = true;
